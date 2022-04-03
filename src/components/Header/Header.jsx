824107be9541ebc5f23b2b5c5f3aeb3f86e656f3
ShavelD <legrand.97@mail.ru>
2022-04-03 11:54:28 +0300

    PEP

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Header() {
+    return (
+        <header className="header">
+            <img
+                src='http://www.dsource.in/sites/default/files/case-study/logo-70-years-indias-independence/download-logo/images/Ashley-logo.png'/>
+        </header>
+    )
+}
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function Profile() {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://jssors8.azureedge.net/demos/image-slider/img/faded-monaco-scenery-evening-dark-picjumbo-com-image.jpg"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>My posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
