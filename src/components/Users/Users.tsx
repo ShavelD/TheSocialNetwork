98bda3f09977d605c7a255fd021b3097bc7cc71d
ShavelD <legrand.97@mail.ru>
2022-10-10 14:22:51 +0300

    62 full.

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from "./Users.module.css";
 import userPhoto from "../assets/images/users.png";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 
 export type UsersType = {
@@ -66,10 +67,30 @@ let Users = (props: PropsType) => {
                        <div>
                            {u.followed
                                ? <button onClick={() => {
-                                   props.unfollow(u.id)
+                                   axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,  {
+                                       withCredentials: true,
+                                       headers: {
+                                         "API-KEY": "10caccf5-2a13-4e49-972f-6c27c112fb00"
+                                       }
+                                   })
+                                       .then(response => {
+                                           if (response.data.resultCode === 0) {
+                                               props.unfollow(u.id)
+                                           }
+                                       })
                                }}>Unfollow</button>
                                : <button onClick={() => {
-                                   props.follow(u.id)
+                                   axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                       withCredentials: true,
+                                       headers: {
+                                           "API-KEY": "10caccf5-2a13-4e49-972f-6c27c112fb00"
+                                       }
+                                   })
+                                       .then(response => {
+                                           if (response.data.resultCode === 0) {
+                                               props.follow(u.id)
+                                           }
+                                       })
                                }}>Follow</button>}
                        </div>
                    </span>
