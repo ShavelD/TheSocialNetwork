11bbf2cc54773f453b3864215648b2cd1154ba4e
ShavelD <legrand.97@mail.ru>
2022-11-25 17:53:57 +0300

    79 full

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {Navbar} from "./components/Navbar/Navbar";
 import {BrowserRouter,Route} from "react-router-dom";
@@ -7,29 +7,33 @@ import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "./redux/redux-store";
-import {CircularProgress} from "@mui/material";
+import {CircularProgress, LinearProgress} from "@mui/material";
 import {ErrorSnackbar} from "./components/ErrorSnackbar/ErrorSnackbar";
+import {getAuthUserData} from "./redux/auth-reducer";
+import {RequestStatusType} from "./redux/app-reducer";
 
 
 
 function App() {
 
-    // const isInitialized = useSelector<AppStateType,boolean>(state => state.app.isInitialized)
-    //
-    // if (!isInitialized) {
-    //     return <div
-    //         style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
-    //         <CircularProgress/>
-    //     </div>
-    // }
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getAuthUserData())
+    },[])
+    const isInitialized = useSelector<AppStateType, boolean>(state => state.app.isInitialized)
+
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+    }
     return (
         <BrowserRouter>
             <div className="app-wrapper">
-                <ErrorSnackbar/>
                 <HeaderContainer/>
-                {/*{status === 'loading' && <LinearProgress/>}*/}
                 <Navbar/>
                 <div className="app-wrapper-content">
                     <Route path='/Dialogs/' render={() => <DialogsContainer/>}/>
@@ -37,6 +41,7 @@ function App() {
                     <Route path='/Users/' render={() => <UsersContainer/>}/>
                     <Route path='/Login/' render={() => <Login/>}/>
                 </div>
+                <ErrorSnackbar/>
             </div>
         </BrowserRouter>
     );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,54 @@
 import React from "react";
 import s from './Header.module.css'
 import {NavLink} from "react-router-dom";
+import {LinearProgress} from "@mui/material";
+import {useSelector} from "react-redux";
+import {AppStateType} from "../../redux/redux-store";
+import {RequestStatusType} from "../../redux/app-reducer";
+import {makeStyles} from "@material-ui/styles";
+
 
 export type PropsType = {
     isLoggedIn: boolean,
     login: null | string,
-    logOutTC: () => void
+    logOutTC: () => void,
 }
 
+
+const useStyles = makeStyles({
+    root: {
+        position: 'absolute',
+        zIndex: 9999,
+        top: 56,
+        width: '1366px',
+        // left: 0,
+        background: 'linear-gradient(45deg, #29f0e1 30%, #d86b0f 90%)',
+        border: 0,
+        borderRadius: 3,
+        // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        // height: 4,
+        // width: '1252px',
+        // padding: '0 0',
+    },
+});
+
 export function Header(props: PropsType) {
+    const status = useSelector<AppStateType, RequestStatusType>((state) => state.app.status)
+    const classes = useStyles();
+
     return (
-        <header className={s.header}>
-            <img
-                src='http://www.dsource.in/sites/default/files/case-study/logo-70-years-indias-independence/download-logo/images/Ashley-logo.png'/>
-            <div className={s.loginBlock}>
-                {
-                    props.isLoggedIn
-                        ?<div>{props.login} $$<button onClick={props.logOutTC}>Log Out</button> </div>
-                        : <NavLink to={'/login'}>Login</NavLink>
-                }
-            </div>
-        </header>
-    )
+        <div className={s.header}>
+            <header >
+                <img
+                    src='http://www.dsource.in/sites/default/files/case-study/logo-70-years-indias-independence/download-logo/images/Ashley-logo.png'/>
+                <div className={s.loginBlock}>{props.isLoggedIn ? <div>{props.login} $$
+                        <button onClick={props.logOutTC}>Log Out</button>
+                    </div>
+                    : <NavLink to={'/login'}>Login</NavLink>}
+                </div>
+            </header>
+    {status === 'loading' && <LinearProgress className={classes.root}/>}
+        </div>
+)
 }
